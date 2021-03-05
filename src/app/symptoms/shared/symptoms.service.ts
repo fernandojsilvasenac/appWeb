5b950a465776826a91bbc1b794b432abc763d598
jsfernando <jsfernando@gmail.com>
2021-03-05 20:57:19 -0300

    symptoms-list routerLink edit/:id, app-routing (symptoms-form/new, symptoms-form/edit/:id), service(updateSymptoms), symptoms-form(edit)

--- src/app/symptoms/shared/symptoms.service.ts
+++ src/app/symptoms/shared/symptoms.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
 import { AngularFireStorage } from '@angular/fire/storage';
-// import { Symptoms } from './interface/symptoms';
 import { Symptoms } from './symptoms';
 import { map } from 'rxjs/operators';
 
@@ -31,30 +30,31 @@ export class SymptomsService {
    }
 
    getById(id: string){ // buscar por Id
-
+      return this.symptomsCollection.doc<Symptoms>(id).valueChanges();
    }
 
    addSymptoms(symptoms: Symptoms){
       // 1 momento
-      // console.log(symptoms)
+      this.afs.collection('symptoms').doc().set(Object.assign({}, symptoms))
+
       // return this.symptomsCollection.add(symptoms);
 
       // 2 momento
       // criar id
-      const id = this.afs.createId();
-      const { name, description } = symptoms;
+      // const id = this.afs.createId();
+      // const { name, description } = symptoms;
 
-      this.afs.collection('symptoms').doc(id).set(
-        {
-          name: name,
-          description: description,
-        }
-      )
+      // this.afs.collection('symptoms').doc(id).set(
+      //   {
+      //     name: name,
+      //     description: description,
+      //   }
+      // )
 
    }
 
-   updateSymptoms(){
-
+   updateSymptoms(symptoms: Symptoms, id: string){
+      this.symptomsCollection.doc<Symptoms>(id).update(Object.assign({}, symptoms));
    }
 
    deleteSymptoms(id: string){
--- src/app/symptoms/symptoms-form/symptoms-form.page.ts
+++ src/app/symptoms/symptoms-form/symptoms-form.page.ts
@@ -12,6 +12,7 @@ import { ToastService } from './../../shared/toast.service';
 export class SymptomsFormPage implements OnInit {
   symptoms: Symptoms;
   private symptomsId: string;
+  title: string;
 
   constructor(private activatedRoute: ActivatedRoute,
               private symptomsService: SymptomsService,
@@ -20,19 +21,42 @@ export class SymptomsFormPage implements OnInit {
 
   ngOnInit() {
     this.symptoms = new Symptoms();
+    this.symptomsId = this.activatedRoute.snapshot.params['id'];
+    this.symptomsId ? this.title = "EDITAR Sintoma" : this.title = "NOVO Sintoma";
+
+    if(this.symptomsId){
+       const subscribe = this.symptomsService.getById(this.symptomsId).subscribe( (data: any) =>{
+        subscribe.unsubscribe();
+        const { name, description, imgUrl, filePath } = data;
+        this.symptoms.name = name;
+        this.symptoms.description = description;
+      })
+    }
+
   }
 
   async onSubmit(){
     console.log(this.symptoms)
-    this.symptomsId = this.activatedRoute.snapshot.params['id'];
+    // this.symptomsId = this.activatedRoute.snapshot.params['id'];
     if (this.symptomsId){
       // update
+      try {
+        await this.symptomsService.updateSymptoms(this.symptoms, this.symptomsId);
+        // mensagem OK
+        this.toast.showMessageBottom('Sintoma alterado com sucesso!!!','success')
+        this.router.navigate(['/symptoms-list']);
+      } catch (error) {
+        // mensagem error
+        this.toast.showMessageTop(error, 'danger');
+        console.log(error);
+      }
+
     } else {
       // add
       try {
         await this.symptomsService.addSymptoms(this.symptoms);
         // mensagem OK
-        this.toast.showMessageBottom('Sintomas inserido com sucesso!!!','success')
+        this.toast.showMessageBottom('Sintoma inserido com sucesso!!!','success')
         this.router.navigate(['/symptoms-list']);
       } catch (error) {
         // mensagem error
