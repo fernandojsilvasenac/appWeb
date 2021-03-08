a281e45b47e830d8b22fb5d377a6043bc962d87c
jsfernando <jsfernando@gmail.com>
2021-03-08 15:01:52 -0300

    added alert services to confirm delete method

---
+++ src/app/shared/alert.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AlertService {
+
+  constructor(private alertController: AlertController) { }
+
+  async showConfirmarExclusão(nome: string, actionRemove: () => void){
+    const alert = await this.alertController.create({
+      header: 'Remover o item? ',
+      message: `Deseja remover o item: ${nome}`,
+      buttons: [
+        {
+          text: 'Cancelar'
+        },
+        {
+          text: 'Remover',
+          handler: () => {
+            actionRemove();
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
+}
